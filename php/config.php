337afefd7108d327a4ab85dd267d39b9beb6cff4
eckmo <morgan.lyndel@gmail.com>
2019-11-30 15:18:36 -0700

    cleaning up the repo

--- php/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-   define('DB_SERVER', 'oniddb.cws.oregonstate.edu');
-   define('DB_USERNAME', 'lambejoh-db');
-   define('DB_PASSWORD', 'VuujWJEbf7hNieto');
-   define('DB_DATABASE', 'lambejoh-db');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-?>
