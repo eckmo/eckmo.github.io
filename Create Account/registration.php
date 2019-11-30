1c7efb21bdd0ef3fe73aa8854d80ebc8c3c7967f
arnoldth <arnoldth@oregonstate.edu>
2019-11-29 20:42:37 -0700

    update registration

--- Create Account/registration.php
+++ Create Account/registration.php
@@ -7,24 +7,26 @@
     $dbuser = 'lambejoh-db';
     $dbpass = 'VuujWJEbf7hNieto';
 
-    $mysqli = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
-    if ($mysqli->connect_errno) {
-        echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-    }
-    echo 'Successfully connected to database!';
+    // $mysqli = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+    // if ($mysqli->connect_errno) {
+    //     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    // }
+    // echo 'Successfully connected to database!';
 
     $sql = “INSERT INTO user (email, username, password) VALUES ($email, $username, $password)”;
     $email = htmlspecialchars($_POST['email']);
     $username = htmlspecialchars($_POST['name']);
     $password = htmlspecialchars($_POST['password']);
 
+    echo $username;
+    echo $email;
 
-    if (mysqli_query($mysqli, $sql)){
-        echo 'successfully added to the database';
-    }
+    // if (mysqli_query($mysqli, $sql)){
+    //     echo 'successfully added to the database';
+    // }
 
     $mysqli->close();
  ?>
- 
+
 </body>
 </html>
