92e1e40516eefd5065ad84e9c83f86dc99ffd9fb
eckmo <morgan.lyndel@gmail.com>
2019-11-30 15:08:58 -0700

    Update login.php

--- Login/php/login.php
+++ Login/php/login.php
@@ -1,4 +1,34 @@
 <!DOCTYPE html>
+<?php
+   include("config.php");
+   session_start();
+
+   if($_SERVER["REQUEST_METHOD"] == "POST") {
+      // username and password sent from form
+
+      $myusername = mysqli_real_escape_string($db,$_POST['username']);
+      $mypassword = mysqli_real_escape_string($db,$_POST['password']);
+
+      $sql = "SELECT id FROM admin WHERE username = '$myusername' and passcode = '$mypassword'";
+      $result = mysqli_query($db,$sql);
+      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+      $active = $row['active'];
+
+      $count = mysqli_num_rows($result);
+
+      // If result matched $myusername and $mypassword, table row must be 1 row
+
+      if($count == 1) {
+         session_register("myusername");
+         $_SESSION['login_user'] = $myusername;
+
+         header("location: welcome.php");
+      }else {
+         $error = "Your Login Name or Password is invalid";
+      }
+   }
+?>
+
 <html lang="en">
 <head>
 	<title>Login V19</title>
@@ -70,35 +100,7 @@
         </div>
     </div>
 
-    <?php
-       include("config.php");
-       session_start();
 
-       if($_SERVER["REQUEST_METHOD"] == "POST") {
-          // username and password sent from form
-
-          $myusername = mysqli_real_escape_string($db,$_POST['username']);
-          $mypassword = mysqli_real_escape_string($db,$_POST['password']);
-
-          $sql = "SELECT id FROM admin WHERE username = '$myusername' and passcode = '$mypassword'";
-          $result = mysqli_query($db,$sql);
-          $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-          $active = $row['active'];
-
-          $count = mysqli_num_rows($result);
-
-          // If result matched $myusername and $mypassword, table row must be 1 row
-
-          if($count == 1) {
-             session_register("myusername");
-             $_SESSION['login_user'] = $myusername;
-
-             header("location: welcome.php");
-          }else {
-             $error = "Your Login Name or Password is invalid";
-          }
-       }
-    ?>
 
 
 	<div class="limiter">
