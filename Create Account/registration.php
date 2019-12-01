d1f6ddd96837b4cdd193b04c35e576c8b963566f
arnoldth <arnoldth@oregonstate.edu>
2019-12-01 09:22:00 -0700

    Connected to database

--- Create Account/registration.php
+++ Create Account/registration.php
@@ -1,32 +1,26 @@
-<html>
-<body>
-
 <?php
     $dbhost = 'oniddb.cws.oregonstate.edu';
     $dbname = 'lambejoh-db';
     $dbuser = 'lambejoh-db';
     $dbpass = 'VuujWJEbf7hNieto';
 
-    // $mysqli = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
-    // if ($mysqli->connect_errno) {
-    //     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-    // }
-    // echo 'Successfully connected to database!';
+    $mysqli = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+    if ($mysqli->connect_errno) {
+       echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    }
 
-    $sql = “INSERT INTO user (email, username, password) VALUES ($email, $username, $password)”;
-    $email = htmlspecialchars($_POST['email']);
-    $username = htmlspecialchars($_POST['name']);
-    $password = htmlspecialchars($_POST['password']);
+    $stmt = $mysqli->prepare("INSERT INTO user (email, user_name, password) VALUES (?, ?, ?)");
+    $stmt->bind_param('sss', $email, $username, $password);
 
-    echo $username;
-    echo $email;
+    $email = ($_POST['email']);
+    $username = ($_POST['name']);
+    $password = ($_POST['password']);
 
-    // if (mysqli_query($mysqli, $sql)){
-    //     echo 'successfully added to the database';
-    // }
+    $stmt->execute();
 
+    $stmt->close();
     $mysqli->close();
- ?>
 
-</body>
-</html>
+    header('Location: ../Login/login.html');
+
+?>
